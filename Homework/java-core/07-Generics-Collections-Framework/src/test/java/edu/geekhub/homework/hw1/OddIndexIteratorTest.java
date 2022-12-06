36f21fda5d5194321b2d366dbcad9066e72f99ad
SopilkoMaxim <max199022@gmail.com>
2022-12-06 16:07:41 +0200

    L#7  made test for OddIndexIterator class

--- Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw1/OddIndexIteratorTest.java
+++ Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw1/OddIndexIteratorTest.java
@@ -1,16 +1,32 @@
 package edu.geekhub.homework.hw1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OddIndexIteratorTest {
+    private OddIndexIterable<Integer> integers;
 
-    @Test
-    void hasNext() {
+    @BeforeEach
+    void setUp() {
+        integers = new OddIndexIterable<>();
     }
 
     @Test
-    void next() {
+    void tryToAddSixElementsAndReturnThree() {
+        int counter =0;
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+
+        for (Integer integer : integers)
+            counter++;
+        assertEquals(3,counter );
     }
+
+
 }
