f14bccc7b9d911e707ef4c0a02b2aaa848c18972
SopilkoMaxim <max199022@gmail.com>
2022-12-13 00:56:30 +0200

    L#8 filled methods

--- Homework/java-core/08-Stream-Api/src/main/java/org/example/hw/Exercises.java
+++ Homework/java-core/08-Stream-Api/src/main/java/org/example/hw/Exercises.java
@@ -1,10 +1,14 @@
 package org.example.hw;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LongSummaryStatistics;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Exercises {
     private static final Cities citiesRepo = new Cities();
+
 
     public Map<String, Long> getCountryCitiesCount() {
         // Find the number of cities of each country (use grouping)
@@ -17,36 +21,86 @@ public class Exercises {
     }
 
     public City mostPopulatedCity() {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+                .max(Comparator.comparing(City::getPopulation))
+                .get();
     }
 
     public City minPopulatedCity() {
-        // Find the min populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+                .min(Comparator.comparing(City::getPopulation))
+                .get();
+
     }
 
     public String mostPopulatedCountry() {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+                .collect(Collectors.groupingBy(City::getCountryCode, Collectors.summingInt(City::getPopulation)))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get()
+                .getKey();
     }
 
     public String minPopulatedCountry() {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+                .collect(Collectors.groupingBy(City::getCountryCode, Collectors.summingInt(City::getPopulation)))
+                .entrySet()
+                .stream()
+                .min(Map.Entry.comparingByValue())
+                .get()
+                .getKey();
+
     }
 
     public Long totalPopulation() {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+                .collect(Collectors.summarizingLong(City::getPopulation))
+                .getSum();
+
     }
 
     public Map<String,Integer> populationOfEachCountry() {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+                .collect(Collectors.groupingBy(City::getCountryCode, Collectors.summingInt(City::getPopulation)));
     }
 
     public Integer populationOfSpecificCountry(String countryCode) {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+                .collect(Collectors.groupingBy(City::getCountryCode, Collectors.summingInt(City::getPopulation)))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getKey().equals(countryCode))
+                .findAny()
+                .get()
+                .getValue();
+
+
     }
 
     public City specificCy(String city) {
-        // Find the most populated city
-    }
+        var cities = citiesRepo.getAllCities();
 
+        return cities.values().stream()
+                .filter(e -> e.getName().equals(city))
+                .findAny()
+                .get();
+
+    }
 
 }
