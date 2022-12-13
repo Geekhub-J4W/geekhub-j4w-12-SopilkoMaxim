b01ae9d2af48405f5b673b2f0075c526417a8557
SopilkoMaxim <max199022@gmail.com>
2022-12-13 12:59:51 +0200

    L#8 Add starter for program. Because unfortunately I didn't understand how to write tests for this methods.

---
+++ Homework/java-core/08-Stream-Api/src/main/java/org/example/hw/Starter.java
@@ -0,0 +1,20 @@
+package org.example.hw;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Starter {
+    public static void main(String[] args) {
+        Exercises exercises = new Exercises();
+
+        System.out.println("Country cities count:\n"+exercises.getCountryCitiesCount());
+        System.out.println("Total population:\n"+ exercises.totalPopulation());
+        System.out.println("Min population:\n"+exercises.minPopulatedCity());
+        System.out.println("Max population city:\n"+exercises.mostPopulatedCity());
+        System.out.println("Population of each country:\n"+exercises.populationOfEachCountry());
+        System.out.println("Min population country:\n"+exercises.minPopulatedCountry());
+        System.out.println("Max population country:\n"+exercises.mostPopulatedCountry());
+        System.out.println("Population of specific country AFG:\n"+exercises.populationOfSpecificCountry("AFG"));
+        System.out.println("Info of specific city Kabul:\n"+exercises.specificCy("Kabul"));
+    }
+}
