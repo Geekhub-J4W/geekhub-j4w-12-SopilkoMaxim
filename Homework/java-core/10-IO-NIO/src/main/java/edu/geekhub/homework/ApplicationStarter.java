a0d5f99943f45baa09d918b92e1e884ed33b59d1
SopilkoMaxim <max199022@gmail.com>
2022-12-22 14:44:05 +0200

    Updating files

--- Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,6 +1,29 @@
 package edu.geekhub.homework;
 
-public class ApplicationStarter {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    // Write code here :)
+public class ApplicationStarter {
+    public static void main(String[] args) {
+        File file = new File("https://github.com/Geekhub-J4W/geekhub-j4w-12-SopilkoMaxim/blob/f30ab478b103b9e962c64a78f39e5f497aa95437/Homework/java-core/10-IO-NIO/src/main/resources/playlist.txt");
+        Scanner input;
+        List<String> songs = new ArrayList<>();
+
+
+        try {
+            input = new Scanner(file);
+            while (input.hasNextLine()) {
+                songs.add(input.nextLine());
+                System.out.println(input.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 }
---
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/Song.java
@@ -0,0 +1,10 @@
+package edu.geekhub.homework;
+
+public record Song(
+        String genre,
+        String group,
+        String album,
+        String name,
+        String url
+) {
+}
