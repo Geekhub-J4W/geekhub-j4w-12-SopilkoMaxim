5a6e366b9c43ad0bd96d1625aff44afd14b487d6
maxsopilko <max199022@gmail.com>
2023-03-19 19:57:03 +0200

    L#19 front-end

--- Homework/java-web/WebApp/src/main/java/edu/geekhub/WebMvcConfig.java
+++ Homework/java-web/WebApp/src/main/java/edu/geekhub/WebMvcConfig.java
@@ -1,11 +1,17 @@
 package edu.geekhub;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ComponentScan("edu.geekhub")
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
