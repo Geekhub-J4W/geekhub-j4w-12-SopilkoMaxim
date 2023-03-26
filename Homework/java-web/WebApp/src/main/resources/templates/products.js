0b50c810408bd0f9e22cda4a48cdd51e1b5bfd10
maxsopilko <max199022@gmail.com>
2023-03-26 14:19:00 +0300

    L#19 delete commented code

--- Homework/java-web/WebApp/src/main/resources/templates/products.js
+++ Homework/java-web/WebApp/src/main/resources/templates/products.js
@@ -7,7 +7,6 @@ const saveProductButton = document.getElementById('save-product-button');
 const imageDialog = document.getElementById('image-dialog');
 const imagePreview = document.getElementById('image-preview');
 
-// Fetch all products from the backend and add them to the table
 fetch('http://localhost:8080/products')
     .then(response => response.json())
     .then(products => {
@@ -29,29 +28,16 @@ fetch('http://localhost:8080/products')
     })
     .catch(error => console.error('Error fetching products:', error));
 
-// Show the product dialog when the "Create Product" button is clicked
 createProductButton.addEventListener('click', () => {
     productForm.reset();
     productDialog.showModal();
 });
 
-// Hide the product dialog when the "Cancel" button is clicked
+
 cancelProductButton.addEventListener('click', () => {
     productDialog.close();
 });
 
-// Show a preview of the selected image when it is uploaded
-// image.addEventListener('change', () => {
-//     const file = image.files[0];
-//     const reader = new FileReader();
-//     reader.addEventListener('load', () => {
-//         imagePreview.src = reader.result;
-//         imageDialog.showModal();
-//     });
-//     reader.readAsDataURL(file);
-// });
-
-// Save the new product or updated product when the "Save" button is clicked
 saveProductButton.addEventListener('click', () => {
     const productData = new FormData(productForm);
     const imageInput = document.getElementById('product-image');
@@ -84,7 +70,6 @@ saveProductButton.addEventListener('click', () => {
         .catch(error => console.error('Error saving product:', error));
 });
 
-// Handle the "Edit" button clicks by showing the product dialog with the current data
 productTable.addEventListener('click', event => {
     const target = event.target;
 
@@ -93,26 +78,22 @@ productTable.addEventListener('click', event => {
         fetch(`http://localhost:8080/products/${id}`)
             .then(response => response.json())
             .then(product => {
-                // Open the dialog
+
                 productDialog.showModal();
 
-                // Set the form values
                 productForm.name.value = product.name;
                 productForm.price.value = product.price;
                 productForm.rating.value = product.rating;
                 productForm.quantityOnStock.value = product.quantityOnStock;
 
-                // Add an event listener for the form submit
                 productForm.addEventListener('submit', event => {
                     event.preventDefault();
 
-                    // Get the form values
                     const name = productForm.name.value;
                     const price = productForm.price.value;
                     const rating = productForm.rating.value;
                     const quantityOnStock = productForm.quantityOnStock.value;
 
-                    // Update the product on the server
                     fetch(`http://localhost:8080/products/${id}`, {
                         method: 'PUT',
                         headers: {
@@ -127,15 +108,13 @@ productTable.addEventListener('click', event => {
                     })
                         .then(response => response.json())
                         .then(product => {
-                            // Close the dialog
                             productDialog.close();
 
-                            // Update the product in the table
-                            // const productRow = document.querySelector(`tr[data-id="${product.id}"]`);
-                            // productRow.querySelector('.product-name').textContent = product.name;
-                            // productRow.querySelector('.product-price').textContent = product.price;
-                            // productRow.querySelector('.product-rating').textContent = product.rating;
-                            // productRow.querySelector('.product-quantity').textContent = product.quantity;
+                            const productRow = document.querySelector(`tr[data-id="${product.id}"]`);
+                            productRow.querySelector('.product-name').textContent = product.name;
+                            productRow.querySelector('.product-price').textContent = product.price;
+                            productRow.querySelector('.product-rating').textContent = product.rating;
+                             productRow.querySelector('.product-quantity').textContent = product.quantity;
                         })
                         .catch(error => {
                             console.error(error);
