e78dc0e489591d46f17a0168fa77029b6eeaeb45
maxsopilko <max199022@gmail.com>
2023-03-26 15:26:12 +0300

    L#19 add starting data

---
+++ Homework/java-web/Domain/src/main/resources/db/flyway/R__Insert_starting_data.sql
@@ -0,0 +1,27 @@
+INSERT INTO customer (id, name, age)
+VALUES
+    (1, 'John Week', 30),
+    (2, 'Agent Smith', 25),
+    (3, 'Bob Marly', 40),
+    (4, 'Emily Davis', 28),
+    (5, 'Mike Tyson', 35),
+    (6, 'Sarah Okonor', 22),
+    (7, 'Bruce Lee', 45),
+    (8, 'Jennifer Lopez', 33),
+    (9, 'Mark Wolberg', 27),
+    (10, 'Jessica Alba', 29);
+
+INSERT INTO product (id, name, price, quantity, rating)
+VALUES
+    (1, 'iPhone 13', 999, 10, 4),
+    (2, 'Samsung Galaxy S22', 899, 15, 4),
+    (3, 'iPad Pro', 1299, 5, 4),
+    (4, 'MacBook Pro', 1999, 3, 4),
+    (5, 'Dell XPS 15', 1499, 8, 4),
+    (6, 'Sony WH-1000XM4', 349, 20, 4),
+    (7, 'Bose QuietComfort 35 II', 299, 30, 4),
+    (8, 'GoPro Hero 10', 399, 12, 4),
+    (9, 'Nintendo Switch', 299, 25, 4),
+    (10, 'Xbox Series X', 499, 18, 4);
+
+
